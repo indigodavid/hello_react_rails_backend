d951a1cf28f2bf0fef5190ea080a7324fb673add
David Vera Castillo <indigodavid@gmail.com>
2022-10-17 21:27:02 -0500

    ✅ Add api methods and endpoints

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
+    @greetings = Greeting.all
+    render json: @greetings
   end
 
   def random_greeting
+    random_number = rand(Greeting.count)
+    @greeting = Greeting.all[random_number].text
+    render json: @greeting
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'greetings#index'
+      resources :greetings, only: [:index]
       get 'hello', to: 'greetings#random_greeting'
     end
   end
